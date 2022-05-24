7ccdacf1876c483694a5e9a835494d96f58ec818
RoblesFloresAaronE80 <105708666+Aaron8090@users.noreply.github.com>
2022-05-24 01:10:27 -0500

    Initial commit

---
+++ app/src/main/java/com/example/tamagochi/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.tamagochi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void darComida(View view) {
+    }
+
+    public void limpiarTama(View view) {
+    }
+
+    public void registroTama(View view) {
+    }
+
+    public void curarTama(View view) {
+    }
+}
