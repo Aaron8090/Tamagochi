dacc4ceec51c07fc276e47e34950394f7f5667e3
RoblesFloresAaronE80 <105708666+Aaron8090@users.noreply.github.com>
2022-05-24 10:12:47 -0500

    Se agrego el activity de consulta

--- app/src/main/java/com/example/tamagochi/MainActivity.java
+++ app/src/main/java/com/example/tamagochi/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,5 +25,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void curarTama(View view) {
+    }
+
+    public void estadoTama(View view) {
     }
 }
